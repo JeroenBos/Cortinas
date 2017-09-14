ac5615eca09a3956d262144037cfe6ed4007f045
JeroenBos <jer123bos@gmail.com>
2017-09-14 13:43:35 +0200

    Further outlining greedy descent

--- greedydescent.py
+++ greedydescent.py
@@ -1,4 +1,4 @@
-# define 'scalar' to be 'float or int'.
+# define 'scalar' to be 'float or int'. For now restricted to 'int'. Scaling can later be implemented to include floats
 # define vector to mean N dimensional scalar vector
 # I want to define a function that takes
 #
@@ -32,12 +32,14 @@
 #    while len(open != 0)
 #        current = open.pop()
 #        closed.add(current)
-#        x_dL_array = get_neighbors(current) # array of tuples of vectors and dL options
-#        for x, dL in x_dL_array:
+#        x_array = get_neighbors(current) # array of tuples of vectors and dL options
+#        for x in x_array:
 #            if not open.contains(x) && not closed.contains(x)
+#                dL = compute_dL(L, x)
 #                c = C(x)
 #                f = F(x, dL, c)
 #                open.add((x, f))
+#        open.sort(key=lambda open_pair: open_pair[1]) # could be omitted through heap structure
 #        l = L(x, True)
 #        yield return l
 #
@@ -52,42 +54,79 @@
 #
 # def get_neighbors(x)
 #    for i in range(0, len(x)):
-#        yield compute_d(x, i, 1)
-#        yield compute_d(x, i, -1)
+#        yield compute_next_x(x, i, 1)
+#        yield compute_next_x(x, i, -1)
 #
 #
 #
 # something wrong here. why return None sometimes? d always exist right? it depends on whether it's in open or closed
 #
-# def compute_d(x, i, direction):
+# def compute_next_x(x, dimension, direction):
 #    if direction != 1 && direction != -1
 #        raise ArgumentError
-#    if i >= len(x)
+#    if dimension >= len(x)
 #        raise ArgumentError
 #
-#    if L(diff(x, i, -1), False) != None:
-#        if L(diff(x, i, -2), False) == None:
-#            return None
-#        else
-#            d = copy(x) # array of scalar options
-#            d[i] = L(diff(x, i, -2), False) - L(diff(x, i, -1), False)
-#            return d
-#    elif L(diff(x, i, 1), False) != None:
-#        if L(diff(x, i, 2), False) == None:
-#            return None
-#        elif
-#            d = copy(x) # array of scalar options
-#            d[i] = L(diff(x, i, 2), False) - L(diff(x, i, 1), False)
-#            return d
-#    else
-#        return None
+#    result = copy(x)
+#    dx_dimension = compute_dimensional_dx(x, dimension, direction)
+#    result[dimension] = result[dimension] + dx_dimension
+#    return result
 #
-# def diff(x, dimension, direction):
+# computes the magnitude of dx, which is in the specified dimension and direction
+# def compute_dimensional_dx(x, dimension, direction):
 #    if direction != 1 && direction != -1 && direction != 2 && direction != -2
 #        raise ArgumentError
 #    if i >= len(x)
 #        raise ArgumentError
-#    raise NotImplementedError
+#
+#    return direction # for now only integer increment TODO: implement scaling to float
+#
+#
+# # estimates L at x
+# # param direction: the direction of dx that resulted in x
+# def estimate_L(x, dimension, direction):
+#    if direction != 1 && direction != -1
+#        raise ArgumentError
+#    if dimension >= len(x)
+#        raise ArgumentError
+#
+#    cached_L = direction => x_ = compute_next_x(x, dimension, direction)
+#                            return x, L(x, False)
+#    if direction == 1:
+#        return fit_estimator(cached_L(-2 * direction), cached_L(-direction), cached_L(direction))
+#    else
+#        return fit_estimator(cached_L(direction), cached_L(-direction), cached_L(-2 * direction))
+#
+#
+# # fits a parabola to the specified points and
+# def fit_estimator((x1, y1), (x2, y2), (x3, y3), x):
+#
+#    if y1 == None:
+#        return linear_fit_estimator((x2, y2), (x3, y3), x)
+#    if y2 == None:
+#        return linear_fit_estimator((x1, y1), (x3, y3), x)
+#    if y3 == None:
+#        return linear_fit_estimator((x1, y1), (x2, y2), x)
+#
+#    D = (x1 - x2) * (x1 - x3) * (x2 - x3);
+#    a = (x3 * (y2 - y1) + x2 * (y1 - y3) + x1 * (y3 - y2)) / D;
+#    b = (x3*x3 * (y1 - y2) + x2*x2 * (y3 - y1) + x1*x1 * (y2 - y3)) / D;
+#    c = (x2 * x3 * (x2 - x3) * y1 + x3 * x1 * (x3 - x1) * y2 + x1 * x2 * (x1 - x2) * y3) / D;
+#
+#    return a * x * x + b * x + c
+#
+#
+# def linear_fit_estimator((x1, y1), (x2, y2), x)
+#
+#    if y1 == None:
+#        return None
+#    if y2 == None:
+#        return None
+#
+#    a = (y2 - y1) / (x2 - x1)
+#    b = y1 - a * x1
+#
+#    return a * x + b
 
 # The float in the scalar must probably become a decimal because I'm gonna need equality comparison on it
 
